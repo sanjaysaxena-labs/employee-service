2fb2fc62a0c5c77060c0639bad4a590d1808fe1d
Sanjay Saxena <sanjaysaxena.aiml@gmail.com>
2026-05-17 10:12:18 +0530

    Initialized spring boot application

---
+++ src/test/java/com/saxena/employee_service/EmployeeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saxena.employee_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
