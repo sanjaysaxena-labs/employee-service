7841a37a562b4d519aac426f75cd27c5c6a19ad2
Sanjay Saxena <sanjaysaxena.aiml@gmail.com>
2026-05-17 12:13:39 +0530

    1. Added swagger configuration in the property file. 2. Created endpoints for employee. 3. Commented Security dependency.

--- src/main/java/com/saxena/employee_service/controller/EmployeeController.java
+++ src/main/java/com/saxena/employee_service/controller/EmployeeController.java
@@ -1,14 +1,36 @@
 package com.saxena.employee_service.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.saxena.employee_service.entity.Employee;
+import com.saxena.employee_service.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class EmployeeController {
 
-    @GetMapping("/")
-    public String welcome() {
-        return "Welcome Employee";
+    @Autowired
+    EmployeeService employeeService;
+
+    @GetMapping("/getAllEmployee")
+    public List<Employee> getAllEmployee() {
+        return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("/employee/{id}")
+    public Employee getEmployeeById(@PathVariable Long id) {
+        return employeeService.getEmployeeById(id);
+    }
+
+    @PostMapping("/employee")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        return employeeService.saveEmployee(employee);
+    }
+
+    @DeleteMapping("employee")
+    public void deleteEmployee(@RequestBody Employee employee) {
+        employeeService.deleteEmployee(employee);
     }
 
 
