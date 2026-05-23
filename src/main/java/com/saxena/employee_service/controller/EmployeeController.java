4d03e282120ca1ce695f78c0c07a85c65b921802
Sanjay Saxena <sanjaysaxena.aiml@gmail.com>
2026-05-23 20:21:06 +0530

    Implement logging.

--- src/main/java/com/saxena/employee_service/controller/EmployeeController.java
+++ src/main/java/com/saxena/employee_service/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.saxena.employee_service.controller;
 import com.saxena.employee_service.entity.Employee;
 import com.saxena.employee_service.service.EmployeeService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,34 +12,38 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 @AllArgsConstructor
+@Slf4j
 public class EmployeeController {
 
     EmployeeService employeeService;
 
     @GetMapping("/employees")
     public List<Employee> getAllEmployee() {
+        log.info("Inside getAllEmployee method");
         return employeeService.getAllEmployees();
     }
 
     @GetMapping("/employee/{id}")
     public Employee getEmployeeById(@PathVariable Long id) {
+        log.info("Inside getEmployeeById method");
         return employeeService.getEmployeeById(id);
     }
 
     @PostMapping("/employee")
     public Employee addEmployee(@RequestBody Employee employee) {
+        log.info("Inside addEmployee method");
         return employeeService.saveEmployee(employee);
     }
 
     @PutMapping("/employee")
     public Employee updateEmployee(@RequestBody Employee employee) {
+        log.info("Inside updateEmployee method");
         return employeeService.updateEmployee(employee);
     }
 
     @DeleteMapping("employee")
     public void deleteEmployee(@RequestBody Employee employee) {
+        log.info("Inside deleteEmployee method");
         employeeService.deleteEmployee(employee);
     }
-
-
 }
